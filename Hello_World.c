a06dd46cb982560ae5fa70904dad65860c5ec029
Abdelrhamn Elgharib <50600176+abdelrhmanelgharib@users.noreply.github.com>
2021-02-15 17:41:29 +0200

    solution of some C problem

---
+++ Hello_World.c
@@ -0,0 +1,16 @@
+/*
+ *  Author: El-Gharib
+ *  Created on: 15/2/2021
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    char Str[100];
+    gets(Str);
+    printf("Hello, World!\n");
+    printf("%s",Str);
+
+    return 0;
+}
