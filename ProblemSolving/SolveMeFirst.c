a04db8a9ed32ca7b61ca152073992507f6859fb0
Abdelrhamn Elgharib <50600176+abdelrhmanelgharib@users.noreply.github.com>
2021-03-04 19:50:17 +0200

    First problem

---
+++ ProblemSolving/SolveMeFirst.c
@@ -0,0 +1,26 @@
+/*
+ *  Author: El-Gharib
+ *  Created on: 4/3/2021
+ *  Version: 1
+ */
+
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int solveMeFirst(int a, int b) {
+    // Hint: Type return a+b; below
+    return a+b;
+ 
+}
+
+int main() {
+    int num1,num2;
+    scanf("%d %d",&num1,&num2);
+    int sum; 
+    sum = solveMeFirst(num1,num2);
+    printf("%d",sum);
+    return 0;
+}
