dc86059943f949f12ffad981c1de0c77ac4132f3
Abdelrhamn Elgharib <50600176+abdelrhmanelgharib@users.noreply.github.com>
2021-06-09 18:51:42 +0200

    Problem Solving

--- ProblemSolving/TimeConversion.c
+++ ProblemSolving/TimeConversion.c
@@ -11,110 +11,104 @@
 #include <stdlib.h>
 #include <string.h>
 
-char* readline();
+char *readline();
 
-/*
- * Complete the timeConversion function below.
- */
-
-/*
- * Please either make the string static or allocate on the heap. For example,
- * static char str[] = "hello world";
- * return str;
- *
- * OR
- *
- * char* str = "hello world";
- * return str;
- *
- */
-char* timeConversion(char* s) {
+char *timeConversion(char *s)
+{
     /*
      * Write your code here.
      */
-    int i=0;
+    int i = 0;
 
-    char* Temp = (char*)malloc(sizeof(char)*11);
+    char *Temp = (char *)malloc(sizeof(char) * 11);
 
     char x = strcmp(s, "12:00:00AM");
 
-    if (s[8] =='P')
+    if (s[8] == 'P')
     {
-        if((s[0]=='1') && (s[1]=='2'))
-        {  
-            s[8]= '\0';
+        if ((s[0] == '1') && (s[1] == '2'))
+        {
+            s[8] = '\0';
         }
         else
         {
+            /* add 12 hours */
             s[0] = s[0] - '0' + '1';
-            s[1] = s[0] - '0' + '2'; 
-            s[8]= '\0';
+            s[1] = s[1] - '0' + '2';
+            s[8] = '\0';
         }
     }
-    else if (s[8]=='A')
+    else if (s[8] == 'A')
     {
-        if ((s[0]=='1') && (s[1]=='2'))
+        if ((s[0] == '1') && (s[1] == '2'))
         {
-            s[0]='0';
-            s[1]='0';
-            
+            s[0] = '0';
+            s[1] = '0';
         }
-        s[8]='\0';
+        s[8] = '\0';
     }
 
-    while (s[i] !='\0')
+    while (s[i] != '\0')
     {
         Temp[i] = s[i];
         i++;
     }
 
     return Temp;
-    
-    
-    
-    
-
 }
 
 int main()
 {
-    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+    FILE *fptr = fopen(getenv("OUTPUT_PATH"), "w");
 
-    char* s = readline();
+    char *s = readline();
 
-    char* result = timeConversion(s);
+    char *result = timeConversion(s);
 
     fprintf(fptr, "%s\n", result);
 
     fclose(fptr);
+    free(result);
 
     return 0;
 }
 
-char* readline() {
+char *readline()
+{
     size_t alloc_length = 1024;
     size_t data_length = 0;
-    char* data = malloc(alloc_length);
+    char *data = malloc(alloc_length);
 
-    while (true) {
-        char* cursor = data + data_length;
-        char* line = fgets(cursor, alloc_length - data_length, stdin);
+    while (true)
+    {
+        char *cursor = data + data_length;
+        char *line = fgets(cursor, alloc_length - data_length, stdin);
 
-        if (!line) { break; }
+        if (!line)
+        {
+            break;
+        }
 
         data_length += strlen(cursor);
 
-        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') { break; }
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n')
+        {
+            break;
+        }
 
         size_t new_length = alloc_length << 1;
         data = realloc(data, new_length);
 
-        if (!data) { break; }
+        if (!data)
+        {
+            break;
+        }
 
         alloc_length = new_length;
     }
 
-    if (data[data_length - 1] == '\n') {
+    if (data[data_length - 1] == '\n')
+    {
         data[data_length - 1] = '\0';
     }
 
