6af2a5c471a8d6424bea3bb392ae557f5d7ea7ed
Abdelrhamn Elgharib <50600176+abdelrhmanelgharib@users.noreply.github.com>
2021-06-09 21:30:43 +0200

    Problem Solving

---
+++ ProblemSolving/DrawingBook.c
@@ -0,0 +1,181 @@
+/*
+ *  Author: El-Gharib
+ *  Created on: 9/6/2021
+ *  Version: 1
+ */
+
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *readline();
+char *ltrim(char *);
+char *rtrim(char *);
+
+int parse_int(char *);
+
+/*
+ * Complete the 'pageCount' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER p
+ */
+
+/*  if book have an odd page number then will open on two pages form the end
+     */
+int pageCount(int n, int p)
+{
+    int min = (n / 2) - (p / 2);
+    if (min > p / 2)
+    {
+        min = p / 2;
+    }
+    return min;
+}
+
+int main()
+{
+    FILE *fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    int n = parse_int(ltrim(rtrim(readline())));
+
+    int p = parse_int(ltrim(rtrim(readline())));
+
+    int result = pageCount(n, p);
+
+    fprintf(fptr, "%d\n", result);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char *readline()
+{
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char *data = malloc(alloc_length);
+
+    while (true)
+    {
+        char *cursor = data + data_length;
+        char *line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line)
+        {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n')
+        {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data)
+        {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n')
+    {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data)
+        {
+            data = '\0';
+        }
+    }
+    else
+    {
+        data = realloc(data, data_length + 1);
+
+        if (!data)
+        {
+            data = '\0';
+        }
+        else
+        {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char *ltrim(char *str)
+{
+    if (!str)
+    {
+        return '\0';
+    }
+
+    if (!*str)
+    {
+        return str;
+    }
+
+    while (*str != '\0' && isspace(*str))
+    {
+        str++;
+    }
+
+    return str;
+}
+
+char *rtrim(char *str)
+{
+    if (!str)
+    {
+        return '\0';
+    }
+
+    if (!*str)
+    {
+        return str;
+    }
+
+    char *end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end))
+    {
+        end--;
+    }
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+int parse_int(char *str)
+{
+    char *endptr;
+    int value = strtol(str, &endptr, 10);
+
+    if (endptr == str || *endptr != '\0')
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}
